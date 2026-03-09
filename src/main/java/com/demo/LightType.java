b6f67169e0923512f2a4920b9ea3508870736186
Withengar-aaa <1664653117@qq.com>
2026-03-09 16:41:44 +0800

    光类型的枚举类

---
+++ src/main/java/com/demo/LightType.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+/**
+ * @author Withengar
+ * 光类型
+ * @see Light
+ * AMBIENT 环境光
+ * POINT 点光源
+ * DIRECTIONAL 方向光
+ */
+public enum LightType
+{
+    AMBIENT,POINT,DIRECTIONAL;
+}
