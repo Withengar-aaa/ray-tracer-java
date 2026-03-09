40f6faa3d836588549332e706451f7a04c66cb03
Withengar-aaa <1664653117@qq.com>
2026-03-09 16:45:25 +0800

    新增向量常用方法

--- src/main/java/com/demo/Vector3D.java
+++ src/main/java/com/demo/Vector3D.java
@@ -27,4 +27,38 @@ public class Vector3D
     {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
+
+
+
+    public static Vector3D normalize(Vector3D v)
+    {
+        double mag = Math.sqrt(dotProduct(v, v));
+
+        return new Vector3D(v.x/mag, v.y/mag, v.z/mag);
+    }
+
+
+    public static double vectorLength(Vector3D vector3D)
+    {
+        return Math.sqrt(dotProduct(vector3D,vector3D));
+    }
+
+
+    public static Vector3D multiply(double k,Vector3D vector3D)
+    {
+        return new Vector3D(k * vector3D.x,k * vector3D.y,k*vector3D.z);
+    }
+
+
+    public static Vector3D add(Vector3D v1,Vector3D v2)
+    {
+        return new Vector3D(v1.x + v2.x,v1.y + v2.y,v1.z + v2.z);
+    }
+
+    public static Point vectorToPoint(Vector3D vector3D)
+    {
+        return new Point(vector3D.x,vector3D.y,vector3D.z);
+    }
+
+
 }
