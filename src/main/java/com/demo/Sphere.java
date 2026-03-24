ac63e1fbbe78770d0dfdd052276dd182dd9aa845
Withengar-aaa <1664653117@qq.com>
2026-03-24 11:01:19 +0800

    增加反射属性

--- src/main/java/com/demo/Sphere.java
+++ src/main/java/com/demo/Sphere.java
@@ -4,6 +4,13 @@ import lombok.Data;
 
 /**
  * @author Withengar
+ * <ul>
+ *     <li>center:球体的球心坐标</li>
+ *     <li>radius:半径</li>
+ *     <li>color:颜色</li>
+ *     <li>specular:光泽度</li>
+ *     <li>reflective:反射度</li>
+ * </ul>
  */
 @Data
 public class Sphere
@@ -16,11 +23,14 @@ public class Sphere
 
     int specular;
 
-    public Sphere( Point center,double radius,Color color,int specular)
+    double reflective;
+
+    public Sphere(Point center, double radius, Color color, int specular, double reflective)
     {
-        this.color = color;
-        this.radius = radius;
         this.center = center;
+        this.radius = radius;
+        this.color = color;
         this.specular = specular;
+        this.reflective = reflective;
     }
 }
