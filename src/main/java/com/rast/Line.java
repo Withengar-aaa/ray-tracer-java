49af0ddf6d747bb1616f03e06a58ab7613d9456b
Withengar-aaa <1664653117@qq.com>
2026-04-23 16:58:28 +0800

    光栅化划线类

---
+++ src/main/java/com/rast/Line.java
@@ -0,0 +1,89 @@
+package com.rast;
+
+import com.demo.Color;
+import com.demo.PixelDrawer;
+import com.demo.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line
+{
+    public void drawLine(Point2D p0,Point2D p1,Color color,PixelDrawer drawer)
+    {
+        drawLine( p0.x(),p0.y(),p1.x(),p1.y(),color,drawer);
+    }
+
+    public List<Integer> interpolate(int i0,int d0,int i1,int d1)
+    {
+        ArrayList<Integer> values = new ArrayList<>();
+
+        float a = (float) (d1 - d0) / (i1 - i0);
+
+        float d = d0;
+
+        for (int i = i0;i <= i1;i++)
+        {
+            values.add((int)d);
+
+            d +=a;
+        }
+
+        return values;
+
+    }
+
+    public void drawLine(int x0, int y0, int x1, int y1, Color color,PixelDrawer drawer)
+    {
+        float dx = x1 - x0;
+
+        float dy = y1 - y0;
+
+        if (Math.abs(dx) > Math.abs(dy))
+        {
+            if (dx<0)
+            {
+                int temp = x0; x0 = x1; x1 = temp;
+                temp = y0; y0 = y1; y1 = temp;
+            }
+
+            List<Integer> ys = interpolate(x0, y0, x1, y1);
+
+            int startX = Math.min(x0,x1);
+
+            int endX = Math.max(x0,x1);
+
+            for (int x = startX;x <= endX;x++)
+            {
+                int idx = x - startX;
+
+                int y = ys.get(idx);
+
+                drawer.writePixel(x,y,color);
+
+            }
+        }
+        else
+        {
+            if (dy<0)
+            {
+                int temp = y0; y0 = y1; y1 = temp;
+                temp = x0; x0 = x1; x1 = temp;
+            }
+
+            List<Integer> xs = interpolate(y0, x0, y1, x1);
+
+            int startY = Math.min(y0,y1);
+
+            int endY = Math.max(y0,y1);
+
+            for (int y = startY;y <= endY;y++)
+            {
+                drawer.writePixel(xs.get(y-startY),y,color);
+            }
+        }
+
+    }
+
+}
+
